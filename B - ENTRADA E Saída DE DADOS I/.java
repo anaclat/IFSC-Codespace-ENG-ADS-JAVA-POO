33a518873588480bf509a4d1a4960dd25adbfa04
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-06-30 21:58:27 +0000

    atividades 30/06

---
+++ B - ENTRADA E Saída DE DADOS I/.java
@@ -0,0 +1,9 @@
+public class B1_7Teste {
+    public static void main(String[] args) {
+        // Trabalhando com Character
+        String palavra = "teste";
+        System.out.println("Maiúsculo: " + palavra.toUpperCase());
+
+    }
+
+}
